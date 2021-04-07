2476884ec503f65c11e1c36f0bf8ff3d46201489
Ling Yang <lingyang@lings-mbp.lan>
2021-04-07 14:19:05 -0700

    first commit

---
+++ Android/VizAssist/app/src/test/java/com/example/vizassist/ImageActionsRobolectricTest.java
@@ -0,0 +1,36 @@
+package com.example.vizassist;
+
+import android.content.Intent;
+import android.os.Build;
+import android.provider.MediaStore;
+
+import com.example.vizassist.imagepipeline.ImageActions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.LOLLIPOP)
+public class ImageActionsRobolectricTest {
+    private final static int TEST_CAMERA_REQUEST_CODE = 1;
+    private final static int TEST_GALLERY_REQUEST_CODE = 1;
+
+    @Test
+    public void cameraActionSendsSystemCameraIntent() {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        ImageActions.startCameraActivity(activity, TEST_CAMERA_REQUEST_CODE);
+    }
+
+    @Test
+    public void galleryActionSendsSystemPickIntent() {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        ImageActions.startGalleryActivity(activity, TEST_GALLERY_REQUEST_CODE);
+    }
+}
